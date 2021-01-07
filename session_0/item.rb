b19ce0910947cd44829f2a1600530440357577cb
Kavya Bhat <kavyabhat@gmail.com>
2021-01-07 11:54:13 +0530

    Changes to item.rb

--- session_0/item.rb
+++ session_0/item.rb
@@ -20,32 +20,27 @@ class Item
   end 
   
   def discounted?
-    if(Time.now>@discount_deadline)
+    if(Time.now >= discount_deadline)
         return false
-    elsif(Time.now<@discount_deadline)
+    elsif(Time.now <= discount_deadline)
         return true
     end
   end
   
   def current_price
     if(discounted?)
-      return @price*(100-@discount_rate)/100
+      return price*(100-discount_rate)/100
     else
-      return @price
+      return price
     end
   end
   
   def self.stock_price_by_category(items)
-     final = {
-       1 => 0,
-       2 => 0,
-       3 => 0,
-       4 => 0
-     }
-     
-     items.each{ |i|
+     final = Hash.new(CATEGORIES.length)
+     final.default = 0
+     items.each do |i|
        final[i.category_id] += i.quantity * i.current_price
-     }  
+     end
      return final
   end
 end
