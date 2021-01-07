f270d6d27fcc801684fb1fd4d2aa69435bd26887
Kavya Bhat <kavyabhat@gmail.com>
2021-01-07 11:11:29 +0530

    Word Processing

--- session_0/word_processing.rb
+++ session_0/word_processing.rb
@@ -2,13 +2,22 @@
 # any non-lower case alphabet (A..Z) to corresponding lower case
 # alphabet
 def lower_case(words)
-  raise NotImplementedError # TODO
+  #raise NotImplementedError # TODO
+  sub = []
+  words.each do |i|
+    sub.append(i.downcase)
+  end
+  return sub
 end
 
 # Similar to `lower_case`, this function modifies the array in-place
 # and does not return any value.
 def lower_case!(words)
-  raise NotImplementedError # TODO
+  #raise NotImplementedError # TODO
+  words.map do |i|
+    i = i.downcase
+  end
+  return nil
 end
 
 # Given a prefix and an array of words, return an array containing
@@ -18,7 +27,14 @@ end
 # words_with_prefix('apple', ['apple', 'ball', 'applesauce']) would
 # return the words 'apple' and 'applesauce'.
 def words_with_prefix(prefix, words)
-  raise NotImplementedError # TODO
+  #raise NotImplementedError # TODO
+  same = []
+  words.each do |i|
+    if(i.start_with?(prefix))
+      same.append(i)
+    end
+  end
+  return same
 end
 
 # The similarity score between two words is defined as the length of
@@ -34,11 +50,37 @@ end
 # The function `similarity_score` takes two words and returns the
 # similarity score (an integer).
 def similarity_score(word_1, word_2)
-  raise NotImplementedError # TODO
+  #raise NotImplementedError # TODO
+  if(word_1.length > word_2.length)
+    l = word_2.length
+  elsif(word_2.length >= word_1.length)
+    l = word_1.length
+  end
+  
+  max = 0
+  l.times do |x|
+    if (word_1[0,x].downcase.eql?(word_2[0,x].downcase))
+      max = x
+    end
+  end
+  return max    
 end
 
 # Given a chosen word and an array of words, return an array of word(s)
 # with the maximum similarity score in the order they appear.
 def most_similar_words(chosen_word, words)
-  raise NotImplementedError # TODO
+  #raise NotImplementedError # TODO
+  max = 0
+  sim = []
+  words.each do |i|
+    check = similarity_score(chosen_word, i)
+    if(check>max)
+       max = check
+       sim.clear() #empty elements which have lower score
+       sim.append(i) #add the newest word
+    elsif(check == max)
+       sim.append(i) #multiple words with same score
+    end
+  end
+  return sim
 end
