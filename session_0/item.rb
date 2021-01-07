d40dd27976f7061e73b43070fb0c76050226575f
Kavya Bhat <kavyabhat@gmail.com>
2021-01-07 18:35:24 +0530

    Final changes, item.rb

--- session_0/item.rb
+++ session_0/item.rb
@@ -36,7 +36,7 @@ class Item
  end
  
  def self.stock_price_by_category(items)
-    final = CATEGORIES.keys.to_h { |key|, [key, 0] }
+    final = CATEGORIES.keys.to_h { |key| [key, 0] }
     items.each do |i|
       final[i.category_id] += i.quantity * i.current_price
     end
